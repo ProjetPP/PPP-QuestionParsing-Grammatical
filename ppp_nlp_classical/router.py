62512391f471f1401f4466c5481107b1b7a5c691
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-10-19 11:18:04 +0200

    Execution of the module creation script.

---
+++ ppp_nlp_classical/router.py
@@ -0,0 +1,12 @@
+"""Router of the module."""
+
+from ppp_core.exceptions import ClientError
+
+class Router:
+    def __init__(self, request):
+        # TODO: Implement this
+        pass
+
+    def answer(self):
+        # TODO: Implement this
+        pass
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ppp_nlp_classical',
+    version='0.1',
+    description='Natural language processing module for the PPP.',
+    url='https://github.com/ProjetPP',
+    author='Projet Pensées Profondes'
+    author_email='ppp2014@listes.ens-lyon.fr',
+    license='MIT',
+    classifiers=[
+        'Environment :: No Input/Output (Daemon)',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Libraries',
+    ],
+    install_requires=[
+        'ppp_datamodel',
+        'ppp_core',
+    ],
+    packages=[
+        'ppp_nlp_classical',
+    ],
+)
