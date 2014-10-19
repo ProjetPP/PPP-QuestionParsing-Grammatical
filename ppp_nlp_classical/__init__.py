bc79686974d8114576c85b127e38afa527f57660
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-10-19 11:40:37 +0200

    First unit test.

--- ppp_nlp_classical/__init__.py
+++ ppp_nlp_classical/__init__.py
@@ -1,12 +1,12 @@
 """Natural language processing module for the PPP."""
 
 from ppp_core import HttpRequestHandler
-from .parsetree_to_triple import Node
-from .requesthandler import RequestHandler
+from .parsetree_to_triple import DependenciesTree
+#from .requesthandler import RequestHandler
 
 def app(environ, start_response):
     """Function called by the WSGI server."""
     return HttpRequestHandler(environ, start_response, RequestHandler) \
             .dispatch()
             
-__all__ = ['Node']
+__all__ = ['DependenciesTree']
