86267c0ecef684530c591216539ecae9e03453ea
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-10-29 11:27:27 +0100

    More renameUnknown tests, and string representation of '??' test.

--- tests/test_triple.py
+++ tests/test_triple.py
@@ -18,12 +18,24 @@ class TripleTests(TestCase):
         self.assertEqual(triple.objectT,"France")        
         self.assertEqual('%s' % triple,"(?8 | President of | France)") 
 
+    def testRenaming(self):
+        triple = Triple(1,2,3)
+        triple.renameUnknown(1,4)
+        triple.renameUnknown(2,4)
+        triple.renameUnknown(3,4)
+        triple.renameUnknown(4,8)
+        self.assertEqual(triple.subjectT,8)
+        self.assertEqual(triple.predicateT,8)
+        self.assertEqual(triple.objectT,8)
+
     def testEasyTriplesBucket(self):
         tp = Triple(4,"President of","France")
         bt = TriplesBucket()
         bt.addTriple(tp)
         bt.renameUnknown(4,8)
         self.assertEqual('%s' % bt,"(?8 | President of | France)")
+        bt.renameUnknown(8,0)
+        self.assertEqual('%s' % bt,"(?? | President of | France)")
 
     def testBuildBucket(self):
         tree = computeTree(data.give_president_of_USA()['sentences'][0])
