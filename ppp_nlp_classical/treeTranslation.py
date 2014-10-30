53fe2cf9f003590dfb496f0a6a6623285d155066
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-10-30 15:34:36 +0100

    [treeTranslation] New small simplification (this code quality evaluation is quite hard).

--- ppp_nlp_classical/treeTranslation.py
+++ ppp_nlp_classical/treeTranslation.py
@@ -3,27 +3,25 @@ import sys
 import ppp_datamodel
 from .tripleProduction import TriplesBucket
 
+def newSubNode(triplesBucket,t):
+    """
+        built the subtree of root t
+    """
+    if isinstance(t,int):
+        sub_t = triplesBucket.extractTriple(t)
+        return newNode(triplesBucket,sub_t)
+    else:
+        return ppp_datamodel.Resource(value=t)
+
 def newNode(triplesBucket,t):
     """
         build a tree of root t, with all the triples of triplesBucket possibles
     """
     if not t:
         return ppp_datamodel.Missing()
-    if isinstance(t.subjectT,int):
-        sub_t = triplesBucket.extractTriple(t.subjectT)
-        subj = newNode(triplesBucket,sub_t)
-    else:
-        subj = ppp_datamodel.Resource(value=t.subjectT)
-    if isinstance(t.predicateT,int):
-        sub_t = triplesBucket.extractTriple(t.predicateT)
-        pred = newNode(triplesBucket,sub_t)
-    else:
-        pred = ppp_datamodel.Resource(value=t.predicateT)
-    if isinstance(t.objectT,int):
-        sub_t = triplesBucket.extractTriple(t.objectT)
-        obj = newNode(triplesBucket,sub_t)
-    else:
-        obj = ppp_datamodel.Resource(value=t.objectT)
+    subj=newSubNode(triplesBucket,t.subjectT)
+    pred=newSubNode(triplesBucket,t.predicateT)
+    obj=newSubNode(triplesBucket,t.objectT)
     return ppp_datamodel.Triple(subject=subj, predicate=pred, object=obj)
 
 def buildTree(triplesBucket):
