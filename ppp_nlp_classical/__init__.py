91a9f4700bfe54dfe1065c077cad274d18bb04c0
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-11-07 14:23:46 +0100

    Copy tests from old branch. Fix tests.

--- ppp_nlp_classical/__init__.py
+++ ppp_nlp_classical/__init__.py
@@ -5,7 +5,7 @@ from .preprocessingMerge import mergeNamedEntityTagChildParent, mergeNamedEntity
 from .preprocessing import DependenciesTree, computeTree
 from .questionIdentify import identifyQuestionWord
 from .dependencyAnalysis import simplify
-from .tripleProduction import buildBucket, Triple, TriplesBucket, tripleProduce1, tripleProduce2, tripleProduce3
+from .tripleProduction import buildBucket, Triple, TriplesBucket, tripleProduce1, tripleProduce2, tripleProduce3, tripleProduce4, tripleProduce5
 from .treeTranslation import buildTree
 
 #_+ from .requesthandler import RequestHandler
@@ -15,4 +15,4 @@ def app(environ, start_response):
     return HttpRequestHandler(environ, start_response, RequestHandler) \
             .dispatch()
 
-__all__ = ['DependenciesTree','computeTree','mergeNamedEntityTagChildParent','mergeNamedEntityTagSisterBrother','mergeNamedEntityTag','simplify', 'identifyQuestionWord','buildBucket','Triple','TriplesBucket', 'tripleProduce1', 'tripleProduce2', 'tripleProduce3','buildTree']
+__all__ = ['DependenciesTree','computeTree','mergeNamedEntityTagChildParent','mergeNamedEntityTagSisterBrother','mergeNamedEntityTag','simplify', 'identifyQuestionWord','buildBucket','Triple','TriplesBucket', 'tripleProduce1', 'tripleProduce2', 'tripleProduce3','tripleProduce4','tripleProduce5','buildTree']
