a21f458f923d09212b8378b3fa7278554c1ef386
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-11-13 13:49:16 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppp_nlp_classical',
-    version='0.2.3',
+    version='0.2.4',
     description='Natural language processing module for the PPP.',
     url='https://github.com/ProjetPP/PPP-NLP-Classical',
     author='Projet Pensées Profondes',
