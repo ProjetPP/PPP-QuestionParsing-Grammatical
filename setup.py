17fbfda392ed052e4737e8b37c43ddb2ec3c787e
Valentin Lorentz <progval@progval.net>
2014-11-20 15:07:53 +0100

    Fix release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppp_questionparsing_grammatical',
-    version='0.3',
+    version='0.3.1',
     description='Natural language processing module for the PPP.',
     url='https://github.com/ProjetPP/PPP-NLP-Classical',
     author='Projet Pensées Profondes',
