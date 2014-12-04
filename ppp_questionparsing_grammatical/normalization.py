0381e6c4cdba77c4020998abd2e257925a00fedb
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-12-04 23:49:57 +0100

    Less words in function body (more easy to read).

--- ppp_questionparsing_grammatical/normalization.py
+++ ppp_questionparsing_grammatical/normalization.py
@@ -1,6 +1,7 @@
 import sys
 import ppp_datamodel
 from .preprocessing import DependenciesTree
+from ppp_datamodel import Resource, Missing, Triple, Last, First, Sort, Intersection, Union
 
 sortTab = {
     # how to sort dependending on the superlative
@@ -10,22 +11,22 @@ sortTab = {
 
 def normalize(tree):
     if tree.child == []: # leaf
-        return ppp_datamodel.Resource(value=tree.getWords())
+        return Resource(value=tree.getWords())
     if tree.child[0].dependency == 'R6': # R6 = superlative, ordinal
         assert len(tree.child) ==1
         try: # <------------------------
-            return ppp_datamodel.Last(list=[ppp_datamodel.Sort(list=[normalize(tree.child[0])],predicate=sortTab[tree.getWords()])]) # last / first
+            return Last(list=[Sort(list=[normalize(tree.child[0])],predicate=sortTab[tree.getWords()])]) # last / first
         except KeyError:
-            return ppp_datamodel.First(list=[ppp_datamodel.Sort(list=[normalize(tree.child[0])],predicate='default')])
+            return First(list=[Sort(list=[normalize(tree.child[0])],predicate='default')])
     if tree.child[0].dependency == 'R7': # R7 = conjunction
         result = []
         for t in tree.child:
             assert t.dependency == 'R7'
             result.append(normalize(t))
         if tree.getWords() == 'and':
-            return ppp_datamodel.Intersection(list=result)
+            return Intersection(list=result)
         if tree.getWords() == 'or':
-            return ppp_datamodel.Union(list=result)
+            return Union(list=result)
     result = []
     for t in tree.child: # R1 ... R5, R8
         assert t.dependency != 'R6' and t.dependency != 'R7'
@@ -33,20 +34,20 @@ def normalize(tree):
             result.append(normalize(t))
         if t.dependency == 'R1': # ou enlever la condition, ça devient R4
             if len(t.child) == 0:
-                result.append(ppp_datamodel.Triple(subject=ppp_datamodel.Resource(value=t.getWords()), predicate=ppp_datamodel.Resource(value=tree.getWords()), object=ppp_datamodel.Missing()))
+                result.append(Triple(subject=Resource(value=t.getWords()), predicate=Resource(value=tree.getWords()), object=Missing()))
             else:
                 result.append(normalize(t))
         if t.dependency == 'R2':
-            result.append(ppp_datamodel.Triple(subject=ppp_datamodel.Missing(), predicate=ppp_datamodel.Resource(value=tree.getWords()), object=normalize(t)))
+            result.append(Triple(subject=Missing(), predicate=Resource(value=tree.getWords()), object=normalize(t)))
         if t.dependency == 'R3':
-            result.append(ppp_datamodel.Triple(subject=ppp_datamodel.Missing(), predicate=normalize(t), object=ppp_datamodel.Resource(value=tree.getWords())))
+            result.append(Triple(subject=Missing(), predicate=normalize(t), object=Resource(value=tree.getWords())))
         if t.dependency == 'R4':
-            result.append(ppp_datamodel.Triple(subject=normalize(t), predicate=ppp_datamodel.Resource(value=tree.getWords()), object=ppp_datamodel.Missing()))
+            result.append(Triple(subject=normalize(t), predicate=Resource(value=tree.getWords()), object=Missing()))
         if t.dependency == 'R5':
-           result.append(ppp_datamodel.Triple(subject=ppp_datamodel.Resource(value=tree.getWords()), predicate=normalize(t), object=ppp_datamodel.Missing()))
+           result.append(Triple(subject=Resource(value=tree.getWords()), predicate=normalize(t), object=Missing()))
         if t.dependency == 'R8':
-            result.append(ppp_datamodel.Resource(value=t.getWords()))
+            result.append(Resource(value=t.getWords()))
     if len(result) == 1:
         return result[0]
     else:
-        return ppp_datamodel.Intersection(list=result)
+        return Intersection(list=result)
