6ddef7869827bf9ee08e84bc54905d87a0e11f9e
Valentin Lorentz <progval@progval.net>
2014-12-09 14:54:22 +0100

    Add LRU cache to the requesthandler.

--- ppp_questionparsing_grammatical/config.py
+++ ppp_questionparsing_grammatical/config.py
@@ -8,7 +8,8 @@ from ppp_libmodule.exceptions import InvalidConfig
 
 
 class Config(BaseConfig):
-    __slots__ = ('corenlp_server')
+    __slots__ = ('corenlp_server', 'cache_size')
     config_path_variable = 'PPP_QUESTIONPARSING_GRAMMATICAL_CONFIG'
     def parse_config(self, data):
         self.corenlp_server = data['corenlp_server']
+        self.cache_size = data.get('cache_size', 0)
--- ppp_questionparsing_grammatical/requesthandler.py
+++ ppp_questionparsing_grammatical/requesthandler.py
@@ -1,8 +1,9 @@
 """Request handler of the module."""
 
 import json
-import jsonrpclib
 import logging
+import functools
+import jsonrpclib
 
 from ppp_datamodel import Sentence, Resource
 from ppp_datamodel.communication import TraceItem, Response
@@ -19,7 +20,18 @@ class StanfordNLP:
         return json.loads(self.server.parse(text))
 stanfordnlp = StanfordNLP(Config().corenlp_server)
 
+def parse(sentence):
+    nonAmbiguousSentence = handler.pull(sentence)
+    result = stanfordnlp.parse(nonAmbiguousSentence)
+    tree = computeTree(result['sentences'][0])
+    handler.push(tree)
+    qw = simplify(tree)
+    return normalize(tree)
+if Config().cache_size:
+    parse = functools.lru_cache(Config().cache_size)(parse)
+
 class RequestHandler:
+    __slots__ = ('request',)
     def __init__(self, request):
         self.request = request
 
@@ -29,12 +41,7 @@ class RequestHandler:
         handler = QuotationHandler()
         sentence = self.request.tree.value
         try:
-            nonAmbiguousSentence = handler.pull(sentence)
-            result = stanfordnlp.parse(nonAmbiguousSentence)
-            tree = computeTree(result['sentences'][0])
-            handler.push(tree)
-            qw = simplify(tree)
-            tree = normalize(tree)
+            tree = parse(sentence)
         except QuotationError: # no logging, the error is between the chair and the keyboard
             return []
         except KeyboardInterrupt:
