1b25379cffdc8ca3d01284df328bb9482bd63746
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-12-09 16:01:00 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppp_questionparsing_grammatical',
-    version='0.4.3',
+    version='0.4.4',
     description='Natural language processing module for the PPP.',
     url='https://github.com/ProjetPP/PPP-QuestionParsing-Grammatical',
     author='Projet Pensées Profondes',
