6000cfb96713088ad89a30d30bf4a851464f883e
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-01-15 17:04:36 +0100

    Run the old tests in deep_tests.

--- run_deep_tests.py
+++ run_deep_tests.py
@@ -9,6 +9,10 @@ import unittest
 os.environ['PPP_QUESTIONPARSING_GRAMMATICAL_CONFIG'] = 'example_config.json'
 
 def main(): # pragma: no cover
+    testsuite = unittest.TestLoader().discover('tests/')
+    results = unittest.TextTestRunner(verbosity=1).run(testsuite)
+    if results.errors or results.failures:
+        exit(1)
     testsuite = unittest.TestLoader().discover('deep_tests/')
     results = unittest.TextTestRunner(verbosity=1).run(testsuite)
     if results.errors or results.failures:
