a074b5f3ad4462085599777473cabb4bf1abf43b
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-01-15 17:17:58 +0100

    assertTrue+isinstance -> assertIsInstance

--- deep_tests/test.py
+++ deep_tests/test.py
@@ -14,50 +14,50 @@ class RequestHandlerTest(PPPTestCase(app)):
     def testBasic(self):
         answer = self.getAnswer("What is the birth date of George Washington?")
         self.assertEquals(len(answer), 1)
-        self.assertTrue(isinstance(answer[0].tree, Triple))
+        self.assertIsInstance(answer[0].tree, Triple)
         subject = answer[0].tree.subject
-        self.assertTrue(isinstance(subject, Resource))
+        self.assertIsInstance(subject, Resource)
         self.assertEquals(subject.value, 'George Washington')
         predicate = answer[0].tree.predicate
-        self.assertTrue(isinstance(predicate, Resource))
+        self.assertIsInstance(predicate, Resource)
         self.assertEquals(predicate.value, 'birth date')
         object = answer[0].tree.object
-        self.assertTrue(isinstance(object, Missing))
+        self.assertIsInstance(object, Missing)
 
     def testNested(self):
         answer = self.getAnswer("When was born the daughters of the wife of the president of the United States?")
         self.assertEquals(len(answer), 1)
         tree = answer[0].tree
-        self.assertTrue(isinstance(tree, Triple))
+        self.assertIsInstance(tree, Triple)
         predicate = tree.predicate
-        self.assertTrue(isinstance(predicate, Resource))
+        self.assertIsInstance(predicate, Resource)
         self.assertEquals(predicate.value, 'birth date')
         object = tree.object
-        self.assertTrue(isinstance(object, Missing))
+        self.assertIsInstance(object, Missing)
         subject = tree.subject
-        self.assertTrue(isinstance(subject, Triple))
+        self.assertIsInstance(subject, Triple)
         tree = tree.subject
         predicate = tree.predicate
-        self.assertTrue(isinstance(predicate, Resource))
+        self.assertIsInstance(predicate, Resource)
         self.assertEquals(predicate.value, 'daughter')
         object = tree.object
-        self.assertTrue(isinstance(object, Missing))
+        self.assertIsInstance(object, Missing)
         subject = tree.subject
-        self.assertTrue(isinstance(subject, Triple))
+        self.assertIsInstance(subject, Triple)
         tree = tree.subject
         predicate = tree.predicate
-        self.assertTrue(isinstance(predicate, Resource))
+        self.assertIsInstance(predicate, Resource)
         self.assertEquals(predicate.value, 'wife')
         object = tree.object
-        self.assertTrue(isinstance(object, Missing))
+        self.assertIsInstance(object, Missing)
         subject = tree.subject
-        self.assertTrue(isinstance(subject, Triple))
+        self.assertIsInstance(subject, Triple)
         tree = tree.subject
         predicate = tree.predicate
-        self.assertTrue(isinstance(predicate, Resource))
+        self.assertIsInstance(predicate, Resource)
         self.assertEquals(predicate.value, 'president')
         object = tree.object
-        self.assertTrue(isinstance(object, Missing))
+        self.assertIsInstance(object, Missing)
         subject = tree.subject
-        self.assertTrue(isinstance(subject, Resource))
+        self.assertIsInstance(subject, Resource)
         self.assertEquals(subject.value, 'United States')
