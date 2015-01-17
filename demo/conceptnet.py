deccbc24ab14188776274e8810c50dad27eeb738
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2015-01-17 18:04:58 +0100

    conceptnet normalize

--- demo/conceptnet.py
+++ demo/conceptnet.py
@@ -3,9 +3,22 @@
 import requests
 import sys
 
+from conceptnet5.nodes import normalized_concept_name
+# https://github.com/commonsense/conceptnet5/blob/master/conceptnet5/nodes.py
+# https://github.com/commonsense/conceptnet5/wiki/API
+
+# How to test in a terminal : 
+#   python3
+#   from conceptnet2 import *
+#   associatedWords('elected','/r/RelatedTo')
+#   normalize('elected')
+
 # Run `./conceptnet.py banana` to search words related to banana.
 
 api='http://conceptnet5.media.mit.edu/data/5.2/search'
+
+def normalize(word):
+    return normalized_concept_name('en', word)
 
 def otherWord(word,edge):
     if edge['startLemmas'] == word:
@@ -16,8 +29,8 @@ def associatedWordsRelation(word,relation):
     """
         Return words related to the given word by the given relations.
     """
-    r = requests.get(api,params={'text':word,'limit':10,'rel':relation}).json()
-    return [otherWord(word,w) for w in r['edges'] if w['score']/r['maxScore']>=0.5]
+    r = requests.get(api,params={'text':word,'limit':5,'rel':relation}).json()
+    return [otherWord(word,w) for w in r['edges']] # if w['score']/r['maxScore']>=0.5]
 
 def associatedWords(word,relations):
     """
@@ -31,4 +44,4 @@ def associatedWords(word,relations):
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit("Syntax: ./%s <word to search>" % sys.argv[0])
-    print(associatedWords(sys.argv[1],{'/r/RelatedTo','/r/IsA','/r/causes'}))
+    print(associatedWords(normalize(sys.argv[1]),{'/r/RelatedTo'}))
