cffac7a1983eb3eea5dc4c36772424b712cf05a4
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2015-01-25 14:53:38 +0100

    fix

--- ppp_questionparsing_grammatical/data/__init__.py
+++ ppp_questionparsing_grammatical/data/__init__.py
@@ -1,6 +1,5 @@
 """Data for the Natural Language Processing module of the PPP."""
 
-from .nounification import nounificationDict
 from .exceptions import GrammaticalError
 
-__all__ = ['nounificationDict','GrammaticalError']
+__all__ = ['GrammaticalError']
