96c3275610a9fa66181c5bbaf389ab3db93d4abb
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-02-07 15:55:43 +0100

    Typo.

--- tests/test_dependencytree.py
+++ tests/test_dependencytree.py
@@ -28,7 +28,7 @@ class DependenciesTreeTests(TestCase):
         correctTree(foo1, {'foo1-1' : foo1, 'bar-2' : bar, 'foo2-3' : foo2})
         self.assertEqual(bar.namedEntityTag, '27')
 
-    def testAddNamedEntityTag1(self):
+    def testAddNamedEntityTag3(self):
         foo1 = DependenciesTree('foo1', 1, namedEntityTag='42')
         foo2 = DependenciesTree('foo2', 3, namedEntityTag='42')
         bar = DependenciesTree('bar', 2, namedEntityTag='undef', dependency = 'amod', parent = foo1)
