50c0a8a91086fd77448f5c07181f54dbe9cea7d8
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2015-02-16 15:17:49 +0100

    remove boostrap corenlp

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
-#git clone https://github.com/ProjetPP/Scripts.git
-#cd Scripts
+git clone https://github.com/ProjetPP/Scripts.git
+cd Scripts
 ./bootstrap_corenlp.sh --nouser
 
 export JAVA_PATH=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
@@ -7,4 +7,4 @@ if [ ! -f $JAVA_PATH ]
 then
     export JAVA_PATH=/usr/lib/jvm/java-8-oracle/bin/java
 fi
-CORENLP="stanford-corenlp-full-*" CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" python3 -m corenlp &
+CORENLP="stanford-corenlp-full-2015-01-30" CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" python3 -m corenlp &
