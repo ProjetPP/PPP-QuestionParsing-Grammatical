8163002ac549e1a66f946337beee1aa16438f638
Valentin Lorentz <progval@progval.net>
2015-02-16 16:06:50 +0100

    Update the Stanford Parser.
    Squashed commit of the following:
    
    commit df6afa7e944e7cee3e0ec0029c38b219a51c3cc7
    Author: Valentin Lorentz <progval@progval.net>
    Date:   Mon Feb 16 09:27:26 2015 +0100
    
        Use Oracle's VM if OpenJDK's is not available.
    
    commit 6c50a518b7b31aba2c2fb434d172f8e253129e3e
    Author: Valentin Lorentz <progval@progval.net>
    Date:   Mon Feb 16 09:18:16 2015 +0100
    
        Fix previous commit (4)
    
    commit 4180721909d6174df3e8b387cff6e8c27f86d2e4
    Author: Valentin Lorentz <progval@progval.net>
    Date:   Mon Feb 16 09:10:20 2015 +0100
    
        Fix previous commit (3)
    
    commit 8fcddda0a4a5c4b730522554e6e4c33d812d2740
    Author: Valentin Lorentz <progval@progval.net>
    Date:   Mon Feb 16 09:08:16 2015 +0100
    
        Fix previous commit (2)
    
    commit 8f77d1312febb928ab433edfa94fe72472fafb0a
    Author: Valentin Lorentz <progval@progval.net>
    Date:   Mon Feb 16 09:06:23 2015 +0100
    
        Fix previous commit
    
    commit dbcad7db86dec75ebd1abc5096812120adfb948a
    Author: Valentin Lorentz <progval@progval.net>
    Date:   Mon Feb 16 09:03:45 2015 +0100
    
        Update the version of the StanfordParser on Travis.

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,10 @@
-git clone https://github.com/ProjetPP/Scripts.git
-cd Scripts
+#git clone https://github.com/ProjetPP/Scripts.git
+#cd Scripts
 ./bootstrap_corenlp.sh --nouser
-CORENLP="stanford-corenlp-full-2014-08-27" CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" python3 -m corenlp &
+
+export JAVA_PATH=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+if [ ! -f $JAVA_PATH ]
+then
+    export JAVA_PATH=/usr/lib/jvm/java-8-oracle/bin/java
+fi
+CORENLP="stanford-corenlp-full-*" CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" python3 -m corenlp &
