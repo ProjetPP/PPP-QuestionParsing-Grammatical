622aa5b7621b4a23825bc86872260c39b98ed056
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-02-19 21:56:26 +0100

    Do not use codes anymore.

--- ppp_questionparsing_grammatical/nounDB.py
+++ ppp_questionparsing_grammatical/nounDB.py
@@ -5,9 +5,6 @@ class Nounificator:
         A class to handle the correspondances from the verbs to the nouns.
     """
 
-    direct = 0
-    inverse = 1
-
     def __init__(self):
         self.verbToNounsDirect = {}
         self.verbToNounsInverse = {}
@@ -43,11 +40,7 @@ class Nounificator:
         pickle.dump(self.verbToNounsInverse, f)
         f.close()
 
-    def _add(self, verb, noun, mapChoice):
-        if mapChoice == self.direct:
-            target = self.verbToNounsDirect
-        elif mapChoice == self.inverse:
-            target = self.verbToNounsInverse
+    def _add(self, verb, noun, target):
         try:
             if not noun in target[verb]:
                 target[verb].append(noun)
@@ -58,33 +51,29 @@ class Nounificator:
         """
             Add the given noun to the direct nounifications of the given verb.
         """
-        self._add(verb, noun, self.direct)
+        self._add(verb, noun, self.verbToNounsDirect)
 
     def addInverse(self, verb, noun):
         """
             Add the given noun to the inverse nounifications of the given verb.
         """
-        self._add(verb, noun, self.inverse)
+        self._add(verb, noun, self.verbToNounsInverse)
 
     def addListDirect(self, verb, nounList):
         """
             Add the given list of nouns to the direct nounifications of the given verb.
         """
-        for t in nounList:
-            self.addDirect(verb, t)
+        for noun in nounList:
+            self.addDirect(verb, noun)
 
     def addListInverse(self, verb, nounList):
         """
             Add the given list of nouns to the inverse nounifications of the given verb.
         """
-        for t in nounList:
-            self.addInverse(verb, t)
+        for noun in nounList:
+            self.addInverse(verb, noun)
 
-    def _remove(self, verb, noun, mapChoice):
-        if mapChoice == self.direct:
-            target = self.verbToNounsDirect
-        elif mapChoice == self.inverse:
-            target = self.verbToNounsInverse
+    def _remove(self, verb, noun, target):
         target[verb].remove(noun)
         if target[verb] == []:
             target.pop(verb)
@@ -93,38 +82,30 @@ class Nounificator:
         """
             Remove the given noun from the direct nounifications of the given verb.
         """
-        self._remove(verb, noun, self.direct)
+        self._remove(verb, noun, self.verbToNounsDirect)
 
     def removeInverse(self, verb, noun):
         """
             Remove the given noun from the inverse nounifications of the given verb.
         """
-        self._remove(verb, noun, self.inverse)
+        self._remove(verb, noun, self.verbToNounsInverse)
 
-    def _removeVerb(self, verb, mapChoice):
-        if mapChoice == self.direct:
-            target = self.verbToNounsDirect
-        elif mapChoice == self.inverse:
-            target = self.verbToNounsInverse
+    def _removeVerb(self, verb, target):
         target.pop(verb)
 
     def removeVerbDirect(self, verb):
         """
             Remove all the direct nounifications of the given verb.
         """
-        self._removeVerb(verb, self.direct)
+        self._removeVerb(verb, self.verbToNounsDirect)
 
     def removeVerbInverse(self, verb):
         """
             Remove all the inverse nounifications of the given verb.
         """
-        self._removeVerb(verb, self.inverse)
+        self._removeVerb(verb, self.verbToNounsInverse)
 
-    def _toNouns(self, verb, mapChoice):
-        if mapChoice == self.direct:
-            target = self.verbToNounsDirect
-        elif mapChoice == self.inverse:
-            target = self.verbToNounsInverse
+    def _toNouns(self, verb, target):
         try:
             return target[verb]
         except KeyError:
@@ -134,13 +115,13 @@ class Nounificator:
         """
             Return the list of direct nounifications of the given noun.
         """
-        return self._toNouns(verb, self.direct)
+        return self._toNouns(verb, self.verbToNounsDirect)
 
     def inverseNouns(self, verb):
         """
             Return the list of inverse nounifications of the given noun.
         """
-        return self._toNouns(verb, self.inverse)
+        return self._toNouns(verb, self.verbToNounsInverse)
 
     def exists(self, verb):
         """
@@ -148,11 +129,11 @@ class Nounificator:
         """
         return verb in self.verbToNounsDirect or verb in self.verbToNounsInverse
 
-    def merge(self, t):
+    def merge(self, other):
         """
             Merge with the given nounificator.
         """
-        for key, value in t.verbToNounsDirect.items():
+        for key, value in other.verbToNounsDirect.items():
             self.addListDirect(key, value)
-        for key, value in t.verbToNounsInverse.items():
+        for key, value in other.verbToNounsInverse.items():
             self.addListInverse(key, value)
