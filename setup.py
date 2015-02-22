696d155f3c8e17b9a531a4cae984dd87567793d9
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2015-02-22 14:46:17 +0100

    fix?

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'ppp_questionparsing_grammatical.data',
     ],
     package_data={
-        'ppp_questionparsing_grammatical': ['data/*.pickle'],
+        'ppp_questionparsing_grammatical': ['data/*.pickle','data/*.txt','data/*.json'],
     },
 )
 
